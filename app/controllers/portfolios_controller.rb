b6edc279571d80654d46d5b6b6eb6d00ab372fac
Mark Franciose <mark.franciose@gmail.com>
2017-08-09 16:31:23 -0500

    build controller actions for portfolio controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,21 +1,29 @@
 class PortfoliosController < ApplicationController
   def new
+    @portfolio = Portfolio.new
   end
 
   def create
+    @portfolio = Portfolio.new(portfolio_params)
   end
 
   def show
+    @portfolio = Portfolio.find(params[:id])
   end
 
   def index
+    @portfolios = Portfolio.all
   end
 
   def destroy
+    portfolio = Portfolio.find(params[:id])
+    portfolio.destroy
+    redirect_to portfolios_path_url
   end
 
   private
 
   def portfolio_params
+    params.require(:portfolio).permit(:name, :user_id)
   end
 end
