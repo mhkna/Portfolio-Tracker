b252759f6f0aee11e801fed31d32ce47771f8002
Mark Franciose <mark.franciose@gmail.com>
2017-08-09 16:13:46 -0500

    add routes for portfolios

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,21 @@
+class PortfoliosController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  private
+
+  def portfolio_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :portfolios, only: [:new, :create, :index, :destroy, :show]
   devise_for :users
   devise_scope :user do
   	root :to => 'devise/sessions#new'
