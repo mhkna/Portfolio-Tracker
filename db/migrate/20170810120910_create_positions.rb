a035be6985a5bf012f7b93bb95507763afd12e4e
fridl8 <tdf970@gmail.com>
2017-08-10 07:11:45 -0500

    Add positions migration

---
+++ db/migrate/20170810120910_create_positions.rb
@@ -0,0 +1,13 @@
+class CreatePositions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :positions do |t|
+    	t.integer :owned_shares, null: false
+    	t.decimal :purchase_price, null: false, precision: 16, scale: 4
+    	t.decimal :sale_price, null: false, precision: 16, scale: 4
+    	t.integer :portfolio_id, null: false
+    	t.integer :stock_id, null: false
+
+    	t.timestamps
+    end
+  end
+end
