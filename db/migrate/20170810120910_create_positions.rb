1577f4bc82fdc08af48f075f20f069255961c9c2
Michael Knapp <email": null>
2017-08-10 12:33:38 -0500

    Position controller, routes and new form created.
    * Ran into issues with sale_price that will come back and fix after lunch. Must also pass in stock symbol.

--- db/migrate/20170810120910_create_positions.rb
+++ db/migrate/20170810120910_create_positions.rb
@@ -3,9 +3,9 @@ class CreatePositions < ActiveRecord::Migration[5.1]
     create_table :positions do |t|
     	t.integer :owned_shares, null: false
     	t.decimal :purchase_price, null: false, precision: 16, scale: 4
-    	t.decimal :sale_price, null: false, precision: 16, scale: 4
+    	t.decimal :sale_price, precision: 16, scale: 4
     	t.integer :portfolio_id, null: false
-    	t.integer :stock_id, null: false
+    	t.integer :stock_id#, null: false
 
     	t.timestamps
     end
