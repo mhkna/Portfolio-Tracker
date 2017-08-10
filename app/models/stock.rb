08786e7002045dbdc8865eecaf41e45ac829bb75
fridl8 <tdf970@gmail.com>
2017-08-10 06:55:14 -0500

    Add current_price/request methods to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,16 @@ class Stock < ApplicationRecord
 	has_many :portfolios, through: :positions
 
 	validates_presence_of :name, :symbol
+
+	def current_price
+		response = current_price_request
+		response["Time Series (1min)"].first[1]["4. close"]
+	end
+
+	private
+	def current_price_request
+		uri = URI.parse("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=#{self.symbol}&interval=1min&apikey=#{ENV["ALPHA_VANTAGE_KEY"]}")
+		request = Net::HTTP.get(uri)
+		JSON.parse(request)
+	end
 end
